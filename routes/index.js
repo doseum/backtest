73f85d7c600434a316c71f136a4afd23a5cba497
doseum <chswoo7@ajou.ac.kr>
2020-05-07 14:10:05 +0900

    bodyparser error fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app= express()
 var router = express.Router();
-var bodyParser=require('body-Parser')
+var bodyParser=require('body-parser')
 
 app.use(bodyParser.urlencoded({extended:true}))
 
